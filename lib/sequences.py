19d17d4c4643867d02be5d8556eba522d018e97c
James <james.ekasiba@student.moringaschool.com>
2023-12-03 21:40:04 +0300

    Completed Assignment

---
+++ lib/sequences.py
@@ -0,0 +1,13 @@
+def remove_duplicates(sequence):
+    seen = set()
+    result = []
+
+    for element in sequence:
+        if element not in seen:
+            seen.add(element)
+            result.append(element)
+
+    return result
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+print(remove_duplicates(input_sequence))
